ad071c195e854949fd341619986c3043579baeb6
mewes4 <mewes4@web.de>
2021-01-12 15:48:27 +0100

    controls are more responsive

--- snake.py
+++ snake.py
@@ -1,10 +1,10 @@
 import pygame 
 import random as r
 
-pygame.init()
+#pygame.init()
 
-win=pygame.display.set_mode((500,500))
-pygame.display.set_caption('mewo snake')
+#win=pygame.display.set_mode((500,500))
+#pygame.display.set_caption('mewo snake')
 
 class Square:
 	def __init__(self,x,y,width):
@@ -68,69 +68,91 @@ def new_random_position(snake):
 		if apple.x==nodes.x and apple.y==nodes.y:
 			new_random_position(snake)
 
-run=True
-first_move=True
-new_direction='stop'
-old_direction='stop'
+names=[]
+quit=False
+while(quit==False):
+	run=True
+	first_move=True
+	new_direction='stop'
+	old_direction='stop'
 
-apple=Square(x=20*r.randint(1,24),y=20*r.randint(1,24),width=20)
+	apple=Square(x=20*r.randint(1,24),y=20*r.randint(1,24),width=20)
 
-my_snake=Snake(x=40,y=40,width=20,vel=20,length=3)
+	my_snake=Snake(x=40,y=40,width=20,vel=20,length=3)
 
-speed=50
+	speed=50
 
-while run:
-	pygame.time.delay(speed) #delay
+	print('type your name:')
+	player=input()
 
-	for event in pygame.event.get(): #quit the game
-		if event.type==pygame.QUIT:
+	pygame.init()
+	win=pygame.display.set_mode((500,500))
+	pygame.display.set_caption('mewo snake')
+
+	while run:
+		pygame.time.delay(speed) #delay
+
+		for event in pygame.event.get(): #quit the game
+			if event.type==pygame.QUIT:
+				run=False
+				pygame.quit()
+		
+		if new_direction !='stop':
+			if collision(my_snake): #checks if the snake collides with itself
+				run=False
+				break
+
+		old_direction=new_direction
+
+		keys=pygame.key.get_pressed() #press keys
+		if keys[pygame.K_LEFT] and old_direction!='right':
+			new_direction='left'
+		if keys[pygame.K_RIGHT]and old_direction!='left':
+			new_direction='right'
+		if keys[pygame.K_UP]and old_direction!='down':
+			new_direction='up'
+		if keys[pygame.K_DOWN]and old_direction!='up':
+			new_direction='down'
+		
+		if my_snake.nodes[len(my_snake.nodes)-1].x>=500 or my_snake.nodes[len(my_snake.nodes)-1].y>=500: #boundaries
 			run=False
-	
-	if new_direction !='stop':
-		if collision(my_snake): #checks if the snake collides with itself
+			break
+		if my_snake.nodes[len(my_snake.nodes)-1].x<=0 or my_snake.nodes[len(my_snake.nodes)-1].y<=0:
 			run=False
 			break
 
-	old_direction=new_direction
+		if eat(my_snake, apple):
+			speed-=1
+			my_snake.length+=1
+			old_x=apple.x
+			old_y=apple.y
+			new_random_position(my_snake)
 
-	keys=pygame.key.get_pressed() #press keys
-	if keys[pygame.K_LEFT] and old_direction!='right':
-		new_direction='left'
-	else:
-		if keys[pygame.K_RIGHT]and old_direction!='left':
-			new_direction='right'
-		else:
-			if keys[pygame.K_UP]and old_direction!='down':
-				new_direction='up'
-			else:
-				if keys[pygame.K_DOWN]and old_direction!='up':
-					new_direction='down'
+		my_snake.step(new_direction)
+
+		win.fill((0,0,0))
+		for node in my_snake.nodes: #draw the snake
+			headx=node.x
+			heady=node.y
+			#print('position: x:{}, y:{}'.format(headx,heady))
+			pygame.draw.rect(win,(0,255,0),(headx,heady,my_snake.width,my_snake.width))
+			pygame.draw.rect(win,(255,0,0),(apple.x,apple.y,apple.width,apple.width)) #draw the apple
+
+		pygame.display.update()
+
+	pygame.quit()
+	print('your score: {} Points!'.format(len(my_snake.nodes)-3))
+	names.append((player,len(my_snake.nodes)-3))
+	s,n = 0,''
+	for name in names:
+		if name[1]>s:
+			s=name[1]
+			n=name[0]
+	print('highscore {} with {} points!'.format(n,s))
+	pygame.time.delay(500)
+	print('retry?(y/n)')
+	retry=input()
+	if retry=='n':
+		pygame.quit()
+		quit=True
 	
-	if my_snake.nodes[len(my_snake.nodes)-1].x>=500 or my_snake.nodes[len(my_snake.nodes)-1].y>=500: #boundaries
-		run=False
-		break
-	if my_snake.nodes[len(my_snake.nodes)-1].x<0 or my_snake.nodes[len(my_snake.nodes)-1].y<0:
-		run=False
-		break
-
-	if eat(my_snake, apple):
-		speed-=1
-		my_snake.length+=1
-		old_x=apple.x
-		old_y=apple.y
-		new_random_position(my_snake)
-
-	my_snake.step(new_direction)
-
-	win.fill((0,0,0))
-	for node in my_snake.nodes: #draw the snake
-		headx=node.x
-		heady=node.y
-		#print('position: x:{}, y:{}'.format(headx,heady))
-		pygame.draw.rect(win,(0,255,0),(headx,heady,my_snake.width,my_snake.width))
-		pygame.draw.rect(win,(255,0,0),(apple.x,apple.y,apple.width,apple.width)) #draw the apple
-
-	pygame.display.update()
-
-print('your score: {} Points!'.format(len(my_snake.nodes)-3))
-pygame.quit()
